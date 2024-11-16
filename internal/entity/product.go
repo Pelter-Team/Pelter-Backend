8e89044826e5001c302f250e3c32b7cf80de72b8
wit03 <witjarukit@gmail.com>
2024-11-17 00:40:29 +0700

    feat: dto conversion function

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -1,6 +1,9 @@
 package entity
 
-import "time"
+import (
+	"Pelter_backend/internal/dto"
+	"time"
+)
 
 type Product struct {
 	ID             uint `gorm:"primary_key"`
@@ -17,4 +20,21 @@ type Product struct {
 	VaccineBookURL *string
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
+}
+
+func (pr *Product) ConvertToProductResponse() dto.ProductResponse {
+	return dto.ProductResponse{
+		ID:             pr.ID,
+		Name:           pr.Name,
+		IsSold:         pr.IsSold,
+		Category:       pr.Category,
+		Subcategory:    pr.Subcategory,
+		Description:    pr.Description,
+		IsVerified:     pr.IsVerified,
+		Price:          pr.Price,
+		ImageURL:       pr.ImageURL,
+		VaccineBookURL: pr.VaccineBookURL,
+		CreatedAt:      pr.CreatedAt,
+		UpdatedAt:      pr.UpdatedAt,
+	}
 }
